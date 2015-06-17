9471c6b3b5c6ce1dbb958660fe6aea3ff50c5b8f
Jan Sorensen <jas@praqma.net>
2015-06-17 23:13:39 +0200

    luci-setup.sh uses sh to get absolute path for LUCI_ROOT

--- luci-setup.sh
+++ luci-setup.sh
@@ -1,7 +1,10 @@
 # Source this file to work in the Luci project
 
 export LUCI_CONFIG=${LUCI_CONFIG:-~/.luci}
-echo "LUCI_CONFIG is set to '$LUCI_CONFIG'"
+echo "LUCI_CONFIG set to '$LUCI_CONFIG'"
 
-export LUCI_ROOT=$(cd "$(dirname "$0")"; pwd)
-echo "LUCI_ROOT is set to '$LUCI_ROOT'"
+local dir=$(dirname $0)
+# The (cd $dir ; pwd) prints absolute path for $dir. Do the cd with sh, in other shells (e.g. zsh) cd can have other
+# side effects
+export LUCI_ROOT=$(sh -c "(cd $dir ; pwd)")
+echo "LUCI_ROOT set to '$LUCI_ROOT'"
