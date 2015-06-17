b8cd137059a26a814a8ec30d0d828b9ff5ceb3f2
hoeghh <heh@praqma.net>
2015-06-17 13:38:54 +0200

    Added variable to define path to zetta-tools config folder

--- luci-setup.sh
+++ luci-setup.sh
@@ -1,4 +1,7 @@
 # Source this file to work in the Luci project
 
+export LUCI_CONFIG=${LUCI_CONFIG:-~/.luci}
+echo "LUCI_CONFIG is set to '$LUCI_CONFIG'"
+
 export LUCI_ROOT=$(cd "$(dirname "$0")"; pwd)
 echo "LUCI_ROOT is set to '$LUCI_ROOT'"
