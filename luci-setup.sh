11f1f341c3a5422ec258ac990e7c3003a546a473
Jan Sorensen <jas@praqma.net>
2015-06-19 14:13:40 +0200

    Define LUCI_DATA

--- luci-setup.sh
+++ luci-setup.sh
@@ -1,10 +1,13 @@
 # Source this file to work in the Luci project
 
 export LUCI_CONFIG=${LUCI_CONFIG:-~/.luci}
-echo "LUCI_CONFIG set to '$LUCI_CONFIG'"
+echo "LUCI_CONFIG set to '$LUCI_CONFIG'. Configuration for Luci."
+
+export LUCI_DATA=${LUCI_DATA:-~/.luci_data}
+echo "LUCI_DATA set to '$LUCI_DATA'. Internal data."
 
 dir=$(dirname $0)
 # The (cd $dir ; pwd) prints absolute path for $dir. Do the cd with sh, in other shells (e.g. zsh) cd can have other
 # side effects
 export LUCI_ROOT=$(sh -c "(cd $dir ; pwd)")
-echo "LUCI_ROOT set to '$LUCI_ROOT'"
+echo "LUCI_ROOT set to '$LUCI_ROOT'. Root of the Luci project source."
