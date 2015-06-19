a564f894d042aee4277c68015ea0d5c7fa46b033
Jan Sorensen <jas@praqma.net>
2015-06-19 16:25:21 +0200

    Make sourcing from other directory work in bash and zsh

--- luci-setup.sh
+++ luci-setup.sh
@@ -6,7 +6,12 @@ echo "LUCI_CONFIG set to '$LUCI_CONFIG'. Configuration for Luci."
 export LUCI_DATA=${LUCI_DATA:-~/.luci_data}
 echo "LUCI_DATA set to '$LUCI_DATA'. Internal data."
 
-dir=$(dirname $0)
+# in bash $0 is bash, in zsh it is the path to this file.
+# Other shells has not been tested, the following assume behaviour like zsh (for simplicity, not because it is likely)
+case $0 in
+    bash) f=$BASH_SOURCE ;;
+    *) f=$0
+esac
 # The (cd $dir ; pwd) prints absolute path for $dir. Do the cd with sh, in other shells (e.g. zsh) cd can have other
 # side effects
 export LUCI_ROOT=$(sh -c "(cd $dir ; pwd)")
