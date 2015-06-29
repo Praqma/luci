ebf0db0d63ac2da2b6ab76c2417ba7cffc1316e0
hoeghh <heh@praqma.net>
2015-06-29 15:42:38 +0200

    Script to create a simple Jenkins job xml output

---
+++ bin/docker-jenkins-job.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#define parameters which are passed in.
+shellCmd=$1
+label=$2
+#define the job.
+cat  << EOF
+<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <scm class="hudson.scm.NullSCM"/>
+  <assignedNode>$label</assignedNode>
+  <canRoam>false</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers/>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>$shellCmd</command>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
+EOF
