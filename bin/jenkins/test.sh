c14e0dc9e11f8e58e247921e8d308423d0756f17
Jan Sorensen <jas@praqma.net>
2015-07-06 12:38:22 +0200

    Script to execute test on Jenkins

---
+++ bin/jenkins/test.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# Execute test on Jenkins
+
+pwd
+
+source luci-setup.sh
+
+./bin/runTests
--- luci-setup.sh
+++ luci-setup.sh
@@ -1,5 +1,21 @@
 # Source this file to work in the Luci project
 
+# Attempt to detect the location of this file
+# In bash $0 is bash when source from shell, otherwise it is the file sourcing this file. $BASH_SOURCE is set to this file
+# In zsh it is the path to this file.
+# Other shells has not been tested
+if [ -n "$BASH_SOURCE" ] ; then
+    f=$BASH_SOURCE
+else
+    # Assume zsh
+    f=$0
+fi
+dir=$(dirname $f)
+# The (cd $dir ; pwd) prints absolute path for $dir. Do the cd with sh, in other shells (e.g. zsh) cd can have other
+# side effects
+export LUCI_ROOT=${LUCI_ROOT:-$(sh -c "(cd $dir ; pwd)")}
+echo "LUCI_ROOT set to '$LUCI_ROOT'. Root of the Luci project source."
+
 if type boot2docker > /dev/null 2>&1 ; then
     usingBoot2docker=true
 else
@@ -11,18 +27,6 @@ echo "LUCI_CONFIG set to '$LUCI_CONFIG'. Configuration for Luci."
 
 export LUCI_DATA=${LUCI_DATA:-~/.luci_data}
 echo "LUCI_DATA set to '$LUCI_DATA'. Internal data."
-
-# in bash $0 is bash, in zsh it is the path to this file.
-# Other shells has not been tested, the following assume behaviour like zsh (for simplicity, not because it is likely)
-case $0 in
-    bash) f=$BASH_SOURCE ;;
-    *) f=$0
-esac
-dir=$(dirname $f)
-# The (cd $dir ; pwd) prints absolute path for $dir. Do the cd with sh, in other shells (e.g. zsh) cd can have other
-# side effects
-export LUCI_ROOT=$(sh -c "(cd $dir ; pwd)")
-echo "LUCI_ROOT set to '$LUCI_ROOT'. Root of the Luci project source."
 
 if [ -z "$LUCI_DOCKER_HOST" ] ; then
     if [ $usingBoot2docker = 'true' ] ; then
