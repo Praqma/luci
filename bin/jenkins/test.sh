d965873fae320cc453c1f669393568994b5da637
Jan Sorensen <jas@praqma.net>
2015-07-10 14:30:50 +0200

    List new docker volumes for executing tests

--- bin/jenkins/test.sh
+++ bin/jenkins/test.sh
@@ -6,12 +6,21 @@ LUCI_ROOT=$(realpath $1)
 
 . $1/luci-setup.sh
 
+docker run -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes > /tmp/luci-volumes.txt
+
 echo "Containers before tests:"
 docker ps -a
 echo "--- end ---"
 
 $LUCI_ROOT/bin/runtests
+rc=$?
 
 echo "Containers after tests:"
 docker ps -a
 echo "--- end ---"
+
+echo "New Docker volumes"
+docker run -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes | grep -Fxv -f /tmp/luci-volumes.txt
+echo "--- end ---"
+
+exit $rc
