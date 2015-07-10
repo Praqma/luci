ae331137d2bbefd80e2be7afdacdecbfdc67369b
Jan Sorensen <jas@praqma.net>
2015-07-10 13:54:04 +0200

    List docker containers before and after running tests

--- bin/jenkins/test.sh
+++ bin/jenkins/test.sh
@@ -2,8 +2,16 @@
 
 # Execute test on Jenkins
 
-LUCI_ROOT=$1
+LUCI_ROOT=$(realpath $1)
 
 . $1/luci-setup.sh
 
+echo "Containers before tests:"
+docker ps -a
+echo "--- end ---"
+
 $LUCI_ROOT/bin/runtests
+
+echo "Containers after tests:"
+docker ps -a
+echo "--- end ---"
