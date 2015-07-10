a7f4e968ad49ec21ff8f80038661a4b002d3020b
Jan Sorensen <jas@praqma.net>
2015-07-10 14:35:49 +0200

    Add --rm to cleanup containers in jenkins/test.sh script

--- bin/jenkins/test.sh
+++ bin/jenkins/test.sh
@@ -6,7 +6,7 @@ LUCI_ROOT=$(realpath $1)
 
 . $1/luci-setup.sh
 
-docker run -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes > /tmp/luci-volumes.txt
+docker run --rm -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes > /tmp/luci-volumes.txt
 
 echo "Containers before tests:"
 docker ps -a
@@ -20,7 +20,7 @@ docker ps -a
 echo "--- end ---"
 
 echo "New Docker volumes"
-docker run -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes | grep -Fxv -f /tmp/luci-volumes.txt
+docker run --rm -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes | grep -Fxv -f /tmp/luci-volumes.txt
 echo "--- end ---"
 
 exit $rc
