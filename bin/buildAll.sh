4e611c6b10031bb21f36ea6261d6809acbe2bda5
Jan Sorensen <jas@praqma.net>
2015-08-05 14:59:49 +0200

    Build luci images when running tests.

--- bin/buildAll.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-docker build -t luci/base:0.1 base/context/
-docker build -t luci/java7:0.1 base/java7/context/
-docker build -t luci/jenkins:0.1 base/java7/jenkins/context
-docker build -t luci/nginx:0.1 base/nginx/context
