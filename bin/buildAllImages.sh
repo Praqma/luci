f66d605e1117ceedf580421b3353ecfd7d8b53a0
Jan Sorensen <jas@praqma.net>
2015-08-07 07:58:31 +0200

    Build images in parallel

--- bin/buildAllImages.sh
+++ bin/buildAllImages.sh
@@ -6,16 +6,27 @@ function build {
     local ver=$2
     echo "*** Building $path ***"
     docker build -t luci/$name:$ver $LUCI_ROOT/src/main/docker/$path/context
-    echo "*** Done with $path ***"
+    local rc=$?
+    echo "*** Done with $path. RC: $rc ***"
     echo ''
 }
 
 build base 0.1
-build base/java7 0.1
-build base/java7/jenkins 0.1
-build base/java7/tomcat7 0.1
-build base/java7/tomcat7/artifactory 0.1
-build base/java7/slave-base 0.1
-build base/java7/slave-base/slave-shell 0.1
-build base/java7/slave-base/slave-gradle 0.1
-build base/nginx 0.1
+build base/nginx 0.1 &
+(
+    build base/java7 0.1
+    build base/java7/jenkins 0.1 &
+    (
+        build base/java7/tomcat7 0.1 
+        build base/java7/tomcat7/artifactory 0.1
+    ) &
+    (
+        build base/java7/slave-base 0.1
+        build base/java7/slave-base/slave-shell 0.1 &
+        build base/java7/slave-base/slave-gradle 0.1 &
+        wait
+    ) &
+    wait
+) &
+wait
+echo "Built all luci images"
