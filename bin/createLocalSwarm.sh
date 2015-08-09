6747f0287e38651852a8dcbc09fe973ef294dc3e
Jan Sorensen <jas@praqma.net>
2015-08-09 17:30:47 +0200

    Mac friendly fixes

--- bin/createLocalSwarm.sh
+++ bin/createLocalSwarm.sh
@@ -1,11 +1,11 @@
 # This script creates a Swarm on a local VirtualBox
 # Still in Beta...
 
-SwarmID=$(docker run swarm create)
+swarmId=$(docker-swarm c)
 
-  echo "SwarmID = $SwarmID"
+echo "swarmId = $swarmId"
 
-  docker-machine create --driver virtualbox luci-swarm-master
+docker-machine create --driver virtualbox luci-swarm-master
 
 docker-machine ssh luci-swarm-master<<SSH
   sudo sh -c 'echo -e " \
@@ -15,9 +15,10 @@ docker-machine ssh luci-swarm-master<<SSH
     DOCKER_TLS=no" > /var/lib/boot2docker/profile'
 SSH
 
-  docker-machine restart luci-swarm-master
+# TODO without -D the machine fails to restart on Jan's macbook
+docker-machine -D restart luci-swarm-master
 
-  docker-machine create --driver virtualbox luci-swarm-node-01
+docker-machine create --driver virtualbox luci-swarm-node-01
 
 docker-machine ssh luci-swarm-node-01<<SSH
   sudo sh -c 'echo -e " \
@@ -27,36 +28,34 @@ docker-machine ssh luci-swarm-node-01<<SSH
     DOCKER_TLS=no" > /var/lib/boot2docker/profile'
 SSH
 
-  docker-machine restart luci-swarm-node-01
+docker-machine -D restart luci-swarm-node-01
 
-  # Get ips of the master node and start swarm
-  masterIP=$(docker-machine ls|grep "swarm-master" |cut -d "/" -f3)
-  echo "masterIP = $masterIP"
+# Get ips of the master node and start swarm
+masterIP=$(docker-machine ls|grep "swarm-master" |cut -d "/" -f3)
+echo "masterIP = $masterIP"
 
-  # Create a swarm manager
-  docker-machine ssh luci-swarm-master "docker run -d -p 3376:2375 swarm manage token://$SwarmID"
+# Create a swarm manager
+docker-machine ssh luci-swarm-master "docker run -d -p 3375:2375 swarm manage token://$swarmId"
 
-  # Create a swarm master node
-  docker-machine ssh luci-swarm-master "docker run -d swarm join --addr=$masterIP token://$SwarmID"
+# Create a swarm master node
+docker-machine ssh luci-swarm-master "docker run -d swarm join --addr=$masterIP token://$swarmId"
 
+# Get ips of the node and start swarm
+node01IP=$(docker-machine ls|grep "swarm-node-01" |cut -d "/" -f3)
+echo "node01IP = $node01IP"
 
-  # Get ips of the node and start swarm
-  node01IP=$(docker-machine ls|grep "swarm-node-01" |cut -d "/" -f3)
-  echo "node01IP = $node01IP"
+# Create a swarm node
+docker-machine ssh luci-swarm-node-01 "docker run -d swarm join --addr=$node01IP token://$swarmId"
 
-  # Create a swarm node
-  docker-machine ssh luci-swarm-node-01 "docker run -d swarm join --addr=$node01IP token://$SwarmID"
+sleep 10
 
-  sleep 10
-
-  managerIP=$(echo $masterIP|cut -d":" -f1):3376
-  echo "
+managerIP=$(echo $masterIP|cut -d":" -f1):3375
+echo "
 Your docker host should point at $managerIP
 "
 
-  # Examples (alter localhost to swarm-master ip, if used)
-  docker -H tcp://$managerIP info
-  #docker -H tcp://$managerIP run -d -p 80:80 nginx
-  #docker -H tcp://$managerIP run -d -p 80:80 nginx
-  #docker -H tcp://$managerIP ps
-
+# Examples (alter localhost to swarm-master ip, if used)
+docker -H tcp://$managerIP --tls=false info
+#docker -H tcp://$managerIP run -d -p 80:80 nginx
+#docker -H tcp://$managerIP run -d -p 80:80 nginx
+#docker -H tcp://$managerIP ps
