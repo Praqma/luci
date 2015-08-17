6fb4c18dfa8e7ea8725e99939d2f9dc20d285acf
Jan Sorensen <jas@praqma.net>
2015-08-17 18:15:53 +0200

    Commit ignore test case
    Needs fixing, should not depend on an existing docker machine

--- buildSrc/src/test/groovy/net/praqma/luci/model/JenkinsModelTest.groovy
+++ buildSrc/src/test/groovy/net/praqma/luci/model/JenkinsModelTest.groovy
@@ -1,12 +1,14 @@
 package net.praqma.luci.model
 
 import net.praqma.luci.docker.DockerHost
+import org.junit.Ignore
 import org.junit.Test
 
 
 class JenkinsModelTest {
 
     @Test
+    @Ignore // TODO do not reference lucitest machine
     void testCreateDataContainer() {
         LuciboxModel box = new LuciboxModel("lucitest")
         box.dockerHost = DockerHost.fromDockerMachine('lucibox')
