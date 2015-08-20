2c68759478467f6849556091b7fea28fc4eddf85
Jan Sorensen <jas@praqma.net>
2015-08-21 01:48:33 +0200

    Basic retry logic for static slave connections

--- buildSrc/src/main/resources/scripts/connectSlave.sh
+++ buildSrc/src/main/resources/scripts/connectSlave.sh
@@ -1,7 +1,13 @@
-#! /bin/bash
+#! /bin/sh
 
 slaveName=$1
 
 slaveJar=/luci/data/jenkinsSlave/slave.jar
 
-/luci/mixins/java/bin/java -jar $slaveJar -jnlpUrl http://master:8080/jenkins/computer/$slaveName/slave-agent.jnlp
+while true ; do
+  echo "Connecting to master"
+  /luci/mixins/java/bin/java -jar $slaveJar -jnlpUrl http://master:8080/jenkins/computer/$slaveName/slave-agent.jnlp
+  echo "Connection to Master terminated! Reconnect attempt in 2 minutes "
+  # Retry logic is also built into java.jar. It is quite exceptional to end up here
+  sleep 120
+done
