814b43dec618dee12a71b67aebc0a85dc37c39e0
Jan Sorensen <jas@praqma.net>
2015-08-20 09:45:04 +0200

    Fix jenkins/test.sh to work with gradle impl

--- bin/jenkins/test.sh
+++ bin/jenkins/test.sh
@@ -4,21 +4,27 @@
 
 LUCI_ROOT=$(realpath $1)
 
-. $1/luci-setup.sh
+cd $LUCI_ROOT
+
+# Volumes before executing luci
+docker run --rm -v /var/lib/docker:/docker debian:jessie ls -1 /docker/volumes > /tmp/luci-volumes.txt
 
 echo "Containers before tests:"
 docker ps -a
 echo "--- end ---"
 
-$LUCI_ROOT/bin/runtests | tee testresults.tap
-rc=$?
+$LUCI_ROOT/bin/buildAllImages.sh
+
+./gradlew luciDemoUp
+sleep 20
+./gradlew luciDemoDestroy
 
 echo "Containers after tests:"
 docker ps -a
 echo "--- end ---"
 
 echo "New Docker volumes"
-docker run --rm -v /var/lib/docker:/docker ubuntu:14.04 ls -1 /docker/volumes | grep -Fxv -f /tmp/luci-volumes.txt
+docker run --rm -v /var/lib/docker:/docker debian:jessie ls -1 /docker/volumes | grep -Fxv -f /tmp/luci-volumes.txt
 echo "--- end ---"
 
 exit $rc
