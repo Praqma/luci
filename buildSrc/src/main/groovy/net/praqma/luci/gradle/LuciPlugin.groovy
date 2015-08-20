1b0299578586a4a8dcdad0c355f17f81f0ced1b5
Jan Sorensen <jas@praqma.net>
2015-08-20 09:45:18 +0200

    Apply base plugin

--- buildSrc/src/main/groovy/net/praqma/luci/gradle/LuciPlugin.groovy
+++ buildSrc/src/main/groovy/net/praqma/luci/gradle/LuciPlugin.groovy
@@ -14,6 +14,7 @@ class LuciPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+        project.apply(plugin: 'base')
         project.extensions.create('luci', LuciExtension, project)
 
         def boxes = project.container(LuciboxModel)
