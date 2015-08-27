b06c88f454e0750a6cbb7a44e8cd70f57c088ed1
Jan Sorensen <jas@praqma.net>
2015-08-27 16:13:09 +0200

    Use @Memorized instead of field

--- buildSrc/src/main/groovy/net/praqma/luci/gradle/LuciExtension.groovy
+++ buildSrc/src/main/groovy/net/praqma/luci/gradle/LuciExtension.groovy
@@ -1,5 +1,6 @@
 package net.praqma.luci.gradle
 
+import groovy.transform.Memoized
 import net.praqma.luci.docker.DockerHost
 import net.praqma.luci.docker.DockerHostImpl
 import org.gradle.api.Project
@@ -8,27 +9,19 @@ class LuciExtension {
 
     private Project project
 
-    DockerHost defaultHost = initDefaultHost()
-
     LuciExtension(Project project) {
         this.project = project
     }
 
+    @Memoized
     DockerHost getDefaultHost() {
-        if (this.@defaultHost == null) {
-            this.@defaultHost = initDefaultHost()
-        }
-        return this.@defaultHost
-    }
-
-    private DockerHost initDefaultHost() {
         DockerHost host = null
         if (project.hasProperty('dockerMachine')) {
             String dockerMachine = project['dockerMachine']
             project.logger.lifecycle("Default dockerhost is '${dockerMachine}'")
             host = DockerHostImpl.fromDockerMachine(dockerMachine)
         } else {
-            host = DockerHostImpl.getDefault()
+            host = DockerHostImpl.default
         }
         return host
     }
